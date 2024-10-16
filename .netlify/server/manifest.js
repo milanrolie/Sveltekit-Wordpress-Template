a2bd1d4723cc4ce6d74302d463f0b03c0bcd5f48
milanrolie <112857190+milanrolie@users.noreply.github.com>
2024-10-16 15:22:50 +0200

    Refactor import paths and update adapter in svelte.config.js

--- .netlify/server/manifest.js
+++ .netlify/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.jviFJsMT.js","app":"_app/immutable/entry/app.f0Govq6S.js","imports":["_app/immutable/entry/start.jviFJsMT.js","_app/immutable/chunks/entry.CRS16ssM.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index._VMp3gXv.js","_app/immutable/entry/app.f0Govq6S.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index.Bl_JVcRZ.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.DWq4iwrX.js","app":"_app/immutable/entry/app.Df33zqFl.js","imports":["_app/immutable/entry/start.DWq4iwrX.js","_app/immutable/chunks/entry.CjFdDHWX.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index._VMp3gXv.js","_app/immutable/entry/app.Df33zqFl.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index.Bl_JVcRZ.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CGvBTWOr.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index.Bl_JVcRZ.js","_app/immutable/chunks/entry.CRS16ssM.js","_app/immutable/chunks/index._VMp3gXv.js"];
+export const imports = ["_app/immutable/nodes/1.CXJXEt7t.js","_app/immutable/chunks/scheduler.BfJEPAwo.js","_app/immutable/chunks/index.Bl_JVcRZ.js","_app/immutable/chunks/entry.CjFdDHWX.js","_app/immutable/chunks/index._VMp3gXv.js"];
 export const stylesheets = [];
 export const fonts = [];
